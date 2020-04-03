a0f52fed091b7d069b9280239f67c6ccccf8b96b
András Guseo <andras@divi-magazine.com>
2020-04-03 11:36:02 +0200

    Extracting hooks class

--- fa-kit.php
+++ fa-kit.php
@@ -23,44 +23,8 @@
 
 namespace TDM\FA_Kit_For_Wp;
 
+include ( 'classes/hooks.class.php' );
 include ( 'classes/actionlinks.class.php' );
 include ( 'classes/settings.class.php' );
-
-if ( ! class_exists( 'Main::class' ) ) {
-	/**
-	 * Main Class
-	 */
-	class Main {
-
-		/**
-		 * Setup the plugin's properties.
-		 */
-		public function __construct() {
-
-			// Load plugin textdomain
-			load_plugin_textdomain( 'tdm-font-awesome-kit-for-wordpress', false, basename( dirname( __FILE__ ) ) . '/languages/' );
-
-			// Add action links
-			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), [ ActionLinks::class, 'action_links' ] );
-			add_filter( 'plugin_row_meta', [ ActionLinks::class, 'donate_button' ], 10, 2 );
-
-			// Initialize plugin options
-			//add_action( 'admin_init', [ Settings::class, 'initialize_plugin_options' ] );
-
-			// Render plugin options page
-			add_action( 'admin_menu', [ Settings::class, 'add_settings_page' ], 99 );
-
-			// Add script to head
-			//add_action( 'wp_head', [ $this, 'child_theme_head_script' ] );
-			//add_action( 'admin_head', [ $this, 'child_theme_head_script' ] );
-
-			// Uninstall
-			//register_uninstall_hook( __FILE__, [ $this, 'uninstall_tdm_fakitforwp' ] );
-			//register_activation_hook( __FILE__, [ $this, 'tdm_fakitforwp_activate' ] );
-		}
-
-
-	}
-}
 
 $fa_kit = new Main();
